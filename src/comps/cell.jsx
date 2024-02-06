226472108e965d5c6005b351e1f8d6a1baf3a65e
EgreChain <155614718+EgreChain@users.noreply.github.com>
2024-02-06 17:04:33 -0500

    updated link

--- src/comps/cell.jsx
+++ src/comps/cell.jsx
@@ -8,8 +8,8 @@ const Cell = () => {
           <div className = "bg-black w-full m-1 md:max-w-[300px] p-4 rounded-sm h-[140px] flex flex-col text-white items-center justify-between">
               <h2 className = "text-2xl font-bold">{project.name}</h2>
               <p  className = "py-2 text-sm">{project.desc}</p>
-              <button className = "my-2 w-full rounded-sm text-black" style = {{background:"linear-gradient(to left, pink, hotpink)"}} >
-              Demo
+            <button className = "my-2 w-full rounded-sm text-black" style = {{background:"linear-gradient(to left, pink, hotpink)"}} >
+              <a href = {project.link}>View Project</a>
             </button>
           </div>
     ))}
