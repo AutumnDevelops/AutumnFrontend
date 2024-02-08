36b9e35b613e2f5ae8799f0278d88df082da48dd
EgreChain <155614718+EgreChain@users.noreply.github.com>
2024-02-08 15:32:44 -0500

    edited some links

--- src/scenes/Header.jsx
+++ src/scenes/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Resume_2024 from "./Resume_2024.pdf"
 const Header = () => {
 
   return (
@@ -11,7 +12,7 @@ const Header = () => {
       </div>
           <h2 className = "bg-black p-2 my-3 rounded-sm text-white text-1xl sm:text-2xl">Frontend Web Developer</h2>
           <div className = "flex w-full sm:w-1/5 justify-center my-3">
-            <a className = "bg-black p-2 mx-2 rounded-sm text-white text-md sm:text-xl hover:bg-[#FF6FB5]" download target = "_blank">Resume</a>
+            <a className = "bg-black p-2 mx-2 rounded-sm text-white text-md sm:text-xl hover:bg-[#FF6FB5]" href = {Resume_2024} download target = "_blank">Resume</a>
             <button className = "bg-black p-2 mx-2 rounded-sm text-white text-md sm:text-xl hover:bg-[#FF6FB5]">
             <a href="#projects_r">Projects</a>
             </button>
