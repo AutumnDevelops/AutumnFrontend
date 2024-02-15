f5661331e9ec526cf71c6874a4c598d1b08dbe6f
EgreChain <155614718+EgreChain@users.noreply.github.com>
2024-02-15 11:37:23 -0500

    updated header

--- src/scenes/Header.jsx
+++ src/scenes/Header.jsx
@@ -12,10 +12,6 @@ const Header = () => {
       </div>
           <h2 className = "bg-black p-2 my-3 rounded-sm text-white text-1xl sm:text-2xl">Frontend Web Developer</h2>
           <div className = "flex w-full sm:w-1/5 justify-center my-3">
-            <a className = "bg-black p-2 mx-2 rounded-sm text-white text-md sm:text-xl hover:bg-[#FF6FB5]"  download target = "_blank">Resume</a>
-            <button className = "bg-black p-2 mx-2 rounded-sm text-white text-md sm:text-xl hover:bg-[#FF6FB5]">
-            <a href="#projects_r">Projects</a>
-            </button>
           </div>
     </header>
   )
