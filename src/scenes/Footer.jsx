e49e9cab058d7e4617415acde01c2bc7000d06db
EgreChain <155614718+EgreChain@users.noreply.github.com>
2024-03-01 04:34:47 -0500

    updated homepage

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -4,7 +4,7 @@ import { FaHeart } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className = "w-[100vw] h-[100px] md:text-md text-[12px] flex justify-center items-center" style={{color: "#b88b6d", textShadow: "#b88b6d 1px 0 1px", WebkitTextStroke: "0.25px #e0c1af"}}>
-      <i className = "px-2"><FaHeart /></i> ~  Copyright 2024 © Created by Autumn Rayne Fitzpatrick <i className = "px-2"><FaHeart /></i> ~ 
+      <i className = "px-2"><FaHeart /></i> ~  Copyright 2022 © Created by Autumn Rayne Fitzpatrick <i className = "px-2"><FaHeart /></i> ~ 
     </footer>
   )
 }
--- src/scenes/Header.jsx
+++ src/scenes/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { FaHeart } from "react-icons/fa";
+import resume from "./General_Resume2024.pdf"
 const Header = () => {
 
   return (
     <header className = "w-4/5 h-[100vh] flex flex-col justify-center items-center" id = "header">
      <h1 className="text-4xl font-bold sm:text-5xl flex flex-col items-center justify-center" style={{fontFamily: "'Croissant One', cursive", color: "#b88b6d", textShadow: "#b88b6d 1px 0 1px", WebkitTextStroke: "0.25px #e0c1af"}}>Autumn Rayne</h1>
      <h2 className="text-4xl font-bold sm:text-5xl flex flex-col items-center justify-center" style={{fontFamily: "'Croissant One', cursive", color: "#b88b6d", textShadow: "#b88b6d 1px 0 1px", WebkitTextStroke: "0.25px #e0c1af"}}>Fitzpatrick</h2>
-          <h2 className = "text-1xl sm:text-2xl mt-6 flex items-center" style={{fontFamily: "'Work Sans', sans-serif", color: "#b88b6d", textShadow: "#b88b6d 1px 0 1px", WebkitTextStroke: "0.25px #e0c1af"}}><i className = "px-2"><FaHeart /></i> ~ Frontend Web Developer ~ <i className = "px-2"><FaHeart /></i></h2>
-          <div className = "flex w-full sm:w-1/5 justify-center my-3">
-          </div>
+          <h2 className = "text-md sm:text-2xl mt-6 flex items-center" style={{fontFamily: "'Work Sans', sans-serif", color: "#b88b6d", textShadow: "#b88b6d 1px 0 1px", WebkitTextStroke: "0.25px #e0c1af"}}><i className = "px-2"><FaHeart /></i> ~ Frontend Web Developer ~ <i className = "px-2"><FaHeart /></i></h2>
+          <ul className = "flex w-full sm:w-1/5 justify-center my-3">
+                  <a href="#projects_c"><li className = "text-xl sm:text-xl flex items-center cursor-point" style={{color: "#b88b6d", textShadow: "#b88b6d 1px 0 1px", WebkitTextStroke: "0.25px #e0c1af"}}><FaHeart />  <span className = "pl-[5px]">Projects</span></li></a>
+                  <a className="icon" href = {resume} download="image"target="_blank"><li className = "cursor-point text-xl sm:text-xl flex items-center pl-[40px]" style={{ color: "#b88b6d", textShadow: "#b88b6d 1px 0 1px", WebkitTextStroke: "0.25px #e0c1af"}}><span className = "pr-[5px]" >Resume </span> <FaHeart /> </li></a>
+          </ul>
     </header>
   )
 }
